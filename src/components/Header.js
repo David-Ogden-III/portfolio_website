6987099f4d403461d3d9ab337dc98c52564d5b60
David Ogden <david-ogden699@hotmail.com>
2023-06-24 15:46:01 -0700

    setup Header still needs links on main items. added social icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,46 @@
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div>
+			<Navbar color='secondary' sticky='top' dark>
+				<NavbarBrand>David Ogden III</NavbarBrand>
+				<Nav>
+					<NavItem>
+						<NavLink style={{ color: 'white' }}>About Me</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink style={{ color: 'white' }}>Skills</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink style={{ color: 'white' }}>Education</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink style={{ color: 'white' }}>Projects</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink style={{ color: 'white' }}>Contact</NavLink>
+					</NavItem>
+				</Nav>
+				<Nav>
+					<NavItem>
+						<NavLink
+							style={{ color: 'white' }}
+							href='https://www.linkedin.com/in/david-o-5a601224a/'>
+							<FaLinkedin />
+						</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink
+							style={{ color: 'white' }}
+							href='https://github.com/David-Ogden-III'>
+							<FaGithub />
+						</NavLink>
+					</NavItem>
+				</Nav>
+			</Navbar>
+		</div>
+	);
 };
 export default Header;

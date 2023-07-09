ce1f6912131361f9555d5e1a4ab77b41044a22eb
David Ogden <david-ogden699@hotmail.com>
2023-07-09 00:53:18 -0700

    removed unnecesary div from HomeComp. added state, buttons that change state and conditionally rendered views in SkillsComp

--- src/components/HomeComp.js
+++ src/components/HomeComp.js
@@ -3,35 +3,31 @@ const HomeComp = () => {
 		<div
 			className='snap-start w-screen h-screen flex  items-center justify-center px-6 sm:px-12 lg:px-20'
 			id='home'>
-			<div
-				className='snap-start h-screen w-full flex items-center justify-center'
-				id='home'>
-				<div>
-					<div className='flex justify-center' style={{ transform: 'none' }}>
-						<img
-							className='xs:w-[220px] sm:w-[45%] md:w-[35%] lg:w=[26%] xl:w-[22%]'
-							alt='no pic yet'
-						/>
-					</div>
-
-					<div className='flex flex-col justify-center items-center'>
-						<h6 className='text-base sm:text-xl md:text-xl lg:text-2xl xl:text-3xl font-ubuntu400'>
-							Hello, I'm
-						</h6>
-						<h1 className='text-5xl xs:text-6xl sm:text-7xl md:text-8xl lg:text-10xl xl:text-[120px] xl:leading-[120px] font-marker'>
-							David
-							<br />
-							<span className='sm:ml-16 -mt-36'>Ogden</span>
-						</h1>
-						<h5 className='text-base md:text-xl lg:text-2xl xl:text-3xl mt-4 md:mt-09 font-ubuntu400'>
-							Full Stack Developer
-						</h5>
-					</div>
-
-					<div
-						className='w-full flex flex-col justify-center items-center mt-7'
-						style={{ opacity: 1 }}></div>
+			<div>
+				<div className='flex justify-center' style={{ transform: 'none' }}>
+					<img
+						className='xs:w-[220px] sm:w-[45%] md:w-[35%] lg:w=[26%] xl:w-[22%]'
+						alt='no pic yet'
+					/>
 				</div>
+
+				<div className='flex flex-col justify-center items-center'>
+					<h6 className='text-base sm:text-xl md:text-xl lg:text-2xl xl:text-3xl font-ubuntu400'>
+						Hello, I'm
+					</h6>
+					<h1 className='text-5xl xs:text-6xl sm:text-7xl md:text-8xl lg:text-10xl xl:text-[120px] xl:leading-[120px] font-marker'>
+						David
+						<br />
+						<span className='sm:ml-16 -mt-36'>Ogden</span>
+					</h1>
+					<h5 className='text-base md:text-xl lg:text-2xl xl:text-3xl mt-4 md:mt-09 font-ubuntu400'>
+						Full Stack Developer
+					</h5>
+				</div>
+
+				<div
+					className='w-full flex flex-col justify-center items-center mt-7'
+					style={{ opacity: 1 }}></div>
 			</div>
 		</div>
 	);
--- src/components/SkillsComp.js
+++ src/components/SkillsComp.js
@@ -1,10 +1,53 @@
+import { useState } from 'react';
+import { Button } from 'reactstrap';
+
 const SkillsComp = () => {
+	const [skills, setSkills] = useState(true);
+
 	return (
 		<div
 			id='skills'
-			className='snap-center w-screen h-screen flex  items-center justify-center px-6 sm:px-12 lg:px-20'>
-			<div>
-				<h4>Skills</h4>
+			className='snap-center w-screen h-screen flex items-center justify-center px-6 sm:px-12 lg:px-20'>
+			<div className='flex flex-col justify-center items-center'>
+				<div>
+					<h4>Skills</h4>
+				</div>
+				<div>
+					{skills ? (
+						<>
+							<Button className='mr-2' color='dark' disabled>
+								Hard Skills
+							</Button>
+							<Button
+								className='ml-2'
+								color='dark'
+								onClick={() => setSkills(false)}>
+								Soft Skills
+							</Button>
+						</>
+					) : (
+						<>
+							<Button
+								className='mr-2'
+								color='dark'
+								onClick={() => setSkills(true)}>
+								Hard Skills
+							</Button>
+							<Button color='dark' className='ml-2' disabled>
+								Soft Skills
+							</Button>
+						</>
+					)}
+				</div>
+				<div>
+					{skills ? (
+						<div>
+							<div className='w-20 h-20 border-2 border-black rounded-lg'></div>
+						</div>
+					) : (
+						<div>Skills is not true</div>
+					)}
+				</div>
 			</div>
 		</div>
 	);

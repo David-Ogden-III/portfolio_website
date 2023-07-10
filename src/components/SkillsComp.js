e7b6a1c21da1b45489c57cbabcc4c99692bf2755
David Ogden <david-ogden699@hotmail.com>
2023-07-09 19:06:10 -0700

    added more tiles and made them size responsively

--- src/components/SkillsComp.js
+++ src/components/SkillsComp.js
@@ -10,9 +10,11 @@ const SkillsComp = () => {
 			className='snap-center w-full h-screen flex items-center justify-center px-6 sm:px-12 lg:px-20'>
 			<div className='flex flex-col justify-center items-center'>
 				<div>
-					<h4>Skills</h4>
+					<h4 className='text-base sm:text-lg text-center xl:text-xl mt-2 md:mt-4 !leading-relaxed xl:w-2/3 font-ubuntu500'>
+						Skills
+					</h4>
 				</div>
-				<div>
+				<div className='font-ubuntu400'>
 					{skills ? (
 						<>
 							<Button className='mr-2' color='dark' disabled>
@@ -39,13 +41,33 @@ const SkillsComp = () => {
 						</>
 					)}
 				</div>
-				<div>
+				<div className='flex flex-col my-4'>
 					{skills ? (
-						<div>
-							<div className='w-20 h-20 border-2 border-black rounded-lg'></div>
-						</div>
+						<>
+							<div className='flex flex-row items-center justify-center'>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+							</div>
+							<div className='flex flex-row items-center justify-center'>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+							</div>
+						</>
 					) : (
-						<div>Skills is not true</div>
+						<>
+							<div className='flex flex-row items-center justify-center'>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+							</div>
+							<div className='flex flex-row items-center justify-center'>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+								<div className='w-16 h-16 sm:w-20 md:w-22 lg:w-25 xl:w-28 sm:h-20 md:h-22 lg:h-25 xl:h-28 border-2 border-black bg-white rounded-lg m-3'></div>
+							</div>
+						</>
 					)}
 				</div>
 			</div>
